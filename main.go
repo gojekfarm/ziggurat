234cfdc753316485cafa99f944586f5c445a0842
shubhang.balkundi <2932-shubhang.balkundi@users.noreply.source.golabs.io>
2020-06-21 16:44:59 +0530

    extract start consumer logic

--- main.go
+++ main.go
@@ -4,72 +4,44 @@ import (
 	"fmt"
 	"github.com/confluentinc/confluent-kafka-go/kafka"
 	"log"
+	"strings"
 	"sync"
 )
 
-func main() {
-	c1, _ := kafka.NewConsumer(&kafka.ConfigMap{
-		"bootstrap.servers": "localhost:9092",
-		"group.id":          "myGroup",
-		"auto.offset.reset": "earliest",
-	})
+type HandlerFunc func(message *kafka.Message)
 
-	c2, _ := kafka.NewConsumer(&kafka.ConfigMap{
-		"bootstrap.servers": "localhost:9092",
-		"group.id":          "myGroup",
-		"auto.offset.reset": "earliest",
-	})
-
+func startConsumers(consumerConfig *kafka.ConfigMap, topics []string, instances int, handlerFunc HandlerFunc) {
 	var wg sync.WaitGroup
-
-	_ = c1.SubscribeTopics([]string{"test-go-topic"}, func(consumer *kafka.Consumer, event kafka.Event) error {
-		fmt.Println("Rebalance called for c1")
-		fmt.Println(consumer.Assignment())
-		return nil
-	})
-
-	_ = c2.SubscribeTopics([]string{"test-go-topic"}, func(consumer *kafka.Consumer, event kafka.Event) error {
-		fmt.Println("Rebalance called for c2")
-		fmt.Println(consumer.Assignment())
-		return nil
-	})
-
-
-	log.Println("starting kafka consumer")
-
-	wg.Add(2)
-	go func() {
-		for {
-			msg, err := c1.ReadMessage(-1)
-			if err == nil {
-				fmt.Println("C1 reading a message")
-				fmt.Printf("Message on parition  [%d]: %s\n", msg.TopicPartition.Partition, string(msg.Value))
-			} else {
-				// The client will automatically try to recover from all errors.
-				fmt.Println("Error code => ", err.(kafka.Error).Code())
-				fmt.Println(kafka.ErrTimedOut)
+	for i := 0; i < instances; i++ {
+		consumer, _ := kafka.NewConsumer(consumerConfig)
+		_ = consumer.SubscribeTopics(topics, nil)
+		log.Printf("%v subscribed to %s topics\n", consumerConfig.Get("group.id", ""), strings.Join(topics, ","))
+		wg.Add(1)
+		go func() {
+			for {
+				msg, err := consumer.ReadMessage(-1)
+				if err != nil && err.(kafka.Error).Code() != kafka.ErrTimedOut {
+					break
+				}
+				if msg != nil {
+					log.Printf("Received message at %s", msg.Timestamp)
+					handlerFunc(msg)
+				}
 			}
-
-		}
-		wg.Done()
-	}()
-
-	go func() {
-		for {
-			msg, err := c2.ReadMessage(-1)
-			if err == nil {
-				fmt.Println("C2 reading a message")
-				fmt.Printf("Message on partition [%d]: %s\n", msg.TopicPartition.Partition, string(msg.Value))
-			} else {
-				// The client will automatically try to recover from all errors.
-				fmt.Println("Error code => ", err.(kafka.Error).Code())
-				fmt.Println(kafka.ErrTimedOut)
-			}
-
-		}
-		wg.Done()
-	}()
-
+			wg.Done()
+		}()
+	}
 	wg.Wait()
+}
+
+func main() {
+	consumerConfig := &kafka.ConfigMap{
+		"bootstrap.servers": "localhost:9092",
+		"group.id":          "myGroup",
+		"auto.offset.reset": "earliest",
+	}
+	startConsumers(consumerConfig, []string{"test-topic"}, 1, func(message *kafka.Message) {
+		fmt.Printf("Recevied message %s", message.Value)
+	})
 
 }
