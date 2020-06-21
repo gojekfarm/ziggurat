2ab202e63f001636831e865f210c9189a8c3faf1
shubhang.balkundi <2932-shubhang.balkundi@users.noreply.source.golabs.io>
2020-06-22 00:02:13 +0530

    extracts starting a single consumer instance to a new func

--- ziggurat/consumer.go
+++ ziggurat/consumer.go
@@ -13,30 +13,36 @@ func createConsumer(consumerConfig *kafka.ConfigMap, topics []string) *kafka.Con
 	return consumer
 }
 
+func startConsumer(topicEntity TopicEntityName, handlerFunc HandlerFunc, wg *sync.WaitGroup, consumer *kafka.Consumer, instanceID string) {
+	go func(c *kafka.Consumer, instanceID string, waitGroup *sync.WaitGroup) {
+		wg.Add(1)
+		defer wg.Done()
+		log.Printf("starting consumer %s %s\n", topicEntity, instanceID)
+		for {
+			msg, err := c.ReadMessage(-1)
+			if err != nil && err.(kafka.Error).Code() != kafka.ErrTimedOut {
+				fmt.Printf("[%s] error: %v\n ", instanceID, err.(kafka.Error).Code())
+				break
+			}
+			if msg != nil {
+				log.Printf("Received message by %s on partition %s-%d\n", instanceID, *msg.TopicPartition.Topic, msg.TopicPartition.Partition)
+				handlerFunc(msg)
+				_, commitErr := c.CommitMessage(msg)
+				if commitErr != nil {
+					log.Printf("error committing message %v\n", commitErr)
+				}
+				log.Printf("committed message successfully\n")
+			}
+		}
+		consumer.Close()
+	}(consumer, instanceID, wg)
+}
+
 func StartConsumers(consumerConfig *kafka.ConfigMap, topicEntity TopicEntityName, topics []string, instances int, handlerFunc HandlerFunc, wg *sync.WaitGroup) {
 	for i := 0; i < instances; i++ {
 		consumer := createConsumer(consumerConfig, topics)
 		groupID, _ := consumerConfig.Get("group.id", "")
-		wg.Add(1)
-		go func(c *kafka.Consumer, instanceID int, waitGroup *sync.WaitGroup) {
-			log.Printf("starting consumer %s-%s-%d\n", topicEntity, groupID, instanceID)
-			for {
-				msg, err := c.ReadMessage(-1)
-				if err != nil && err.(kafka.Error).Code() != kafka.ErrTimedOut {
-					fmt.Printf("error: %v ", err.(kafka.Error).Code())
-					break
-				}
-				if msg != nil {
-					log.Printf("Received message by %s-%d on partition %v-%d\n", groupID, instanceID, *msg.TopicPartition.Topic, msg.TopicPartition.Partition)
-					handlerFunc(msg)
-					_, commitErr := c.CommitMessage(msg)
-					if commitErr != nil {
-						log.Printf("error committing message %v\n", commitErr)
-					}
-					log.Printf("committed message successfully\n")
-				}
-			}
-			wg.Done()
-		}(consumer, i, wg)
+		instanceID := fmt.Sprintf("%s-%s-%d", topicEntity, groupID, i)
+		startConsumer(topicEntity, handlerFunc, wg, consumer, instanceID)
 	}
 }
