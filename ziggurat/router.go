22179e006721b3fc3407cdb0a6d851c3ff8a5c29
shubhang.balkundi <2932-shubhang.balkundi@users.noreply.source.golabs.io>
2020-06-21 21:28:22 +0530

    adds provision to supply config from start method

--- ziggurat/router.go
+++ ziggurat/router.go
@@ -1,29 +1,62 @@
 package ziggurat
 
-import "github.com/confluentinc/confluent-kafka-go/kafka"
+import (
+	"fmt"
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"strings"
+	"sync"
+)
 
-type TopicEntityHandlerMap = map[string]TopicEntity
-
-type TopicEntity struct {
-	topicEntity string
-	groupId     string
-	handlerFunc func(message kafka.Message)
+type topicEntity struct {
+	groupId          string
+	handlerFunc      HandlerFunc
+	consumer         *kafka.Consumer
+	bootstrapServers string
+	originTopics     []string
 }
 
+type TopicEntityHandlerMap = map[string]topicEntity
+
+type InstanceCount = map[string]int
+
 type StreamRouter struct {
-	handlerFunctionMap map[string]TopicEntity
+	handlerFunctionMap map[TopicEntityName]*topicEntity
+}
+
+func newConsumerConfig() *kafka.ConfigMap {
+	return &kafka.ConfigMap{
+		"bootstrap.servers": "localhost:9092",
+		"group.id":          "myGroup",
+		"auto.offset.reset": "earliest",
+	}
 }
 
 func NewStreamRouter() *StreamRouter {
 	return &StreamRouter{
-		handlerFunctionMap: make(TopicEntityHandlerMap),
+		handlerFunctionMap: make(map[TopicEntityName]*topicEntity),
 	}
 }
 
-func (sr *StreamRouter) HandlerFunc(topicEntity string, groupId string, handlerFn func(message kafka.Message)) {
-	sr.handlerFunctionMap[topicEntity] = TopicEntity{
-		topicEntity: topicEntity,
-		groupId:     groupId,
-		handlerFunc: handlerFn,
+func (sr *StreamRouter) HandlerFunc(topicEntityName TopicEntityName, handlerFn HandlerFunc) {
+	sr.handlerFunctionMap[topicEntityName] = &topicEntity{handlerFunc: handlerFn}
+}
+
+func makeKV(key string, value string) string {
+	return fmt.Sprintf("%s=%s", key, value)
+}
+
+func (sr *StreamRouter) Start(srConfig StreamRouterConfigMap) {
+	var wg sync.WaitGroup
+	hfMap := sr.handlerFunctionMap
+	for topicEntityName, topicEntity := range hfMap {
+		streamRouterCfg := srConfig[topicEntityName]
+		consumerConfig := newConsumerConfig()
+		bootstrapServers := makeKV("bootstrap.servers", strings.Join(streamRouterCfg.BootstrapServers, ","))
+		groupID := makeKV("group.id", streamRouterCfg.GroupID)
+
+		consumerConfig.Set(bootstrapServers)
+		consumerConfig.Set(groupID)
+		StartConsumers(consumerConfig, topicEntityName, streamRouterCfg.OriginTopics, streamRouterCfg.InstanceCount, topicEntity.handlerFunc, &wg)
 	}
+	wg.Wait()
 }
