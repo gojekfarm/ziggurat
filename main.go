9df10ab435619960a93f327924c2f2c334eb7fbe
shubhang.balkundi <2932-shubhang.balkundi@users.noreply.source.golabs.io>
2020-06-21 19:44:35 +0530

    fixes imports

--- main.go
+++ main.go
@@ -3,36 +3,8 @@ package main
 import (
 	"fmt"
 	"github.com/confluentinc/confluent-kafka-go/kafka"
-	"log"
-	"strings"
-	"sync"
+	"github.com/gojek/ziggurat"
 )
-
-type HandlerFunc func(message *kafka.Message)
-
-func startConsumers(consumerConfig *kafka.ConfigMap, topics []string, instances int, handlerFunc HandlerFunc) {
-	var wg sync.WaitGroup
-	for i := 0; i < instances; i++ {
-		consumer, _ := kafka.NewConsumer(consumerConfig)
-		_ = consumer.SubscribeTopics(topics, nil)
-		log.Printf("%v subscribed to %s topics\n", consumerConfig.Get("group.id", ""), strings.Join(topics, ","))
-		wg.Add(1)
-		go func() {
-			for {
-				msg, err := consumer.ReadMessage(-1)
-				if err != nil && err.(kafka.Error).Code() != kafka.ErrTimedOut {
-					break
-				}
-				if msg != nil {
-					log.Printf("Received message at %s", msg.Timestamp)
-					handlerFunc(msg)
-				}
-			}
-			wg.Done()
-		}()
-	}
-	wg.Wait()
-}
 
 func main() {
 	consumerConfig := &kafka.ConfigMap{
@@ -40,8 +12,8 @@ func main() {
 		"group.id":          "myGroup",
 		"auto.offset.reset": "earliest",
 	}
-	startConsumers(consumerConfig, []string{"test-topic"}, 1, func(message *kafka.Message) {
-		fmt.Printf("Recevied message %s", message.Value)
+	ziggurat.StartConsumers(consumerConfig, []string{"test-topic"}, 3, func(message *kafka.Message) {
+		fmt.Printf("Recevied message [ %s %s ]\n", message.Key, message.Value)
 	})
 
 }
