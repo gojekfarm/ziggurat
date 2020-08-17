c0f28a1ff44f24bac51914f9b88a7818968d5731
shubhang.balkundi <2932-shubhang.balkundi@users.noreply.source.golabs.io>
2020-08-17 09:47:38 +0530

    fixes retry payload

--- ziggurat/retrier.go
+++ ziggurat/retrier.go
@@ -3,12 +3,14 @@ package ziggurat
 type RetryPayload struct {
 	MessageValueBytes []byte
 	MessageKeyBytes   []byte
-	RetryAttributes   map[string]interface{}
+	TopicEntity       string
+	RetryCount        int
+	MessageTTL        string
 }
 
 type MessageRetrier interface {
 	Start(config Config, streamRoutes TopicEntityHandlerMap) error
-	Retry(payload RetryPayload) error
+	Retry(config Config, payload RetryPayload) error
 	Stop() error
 	Consume(config Config, streamRoutes TopicEntityHandlerMap)
 }
