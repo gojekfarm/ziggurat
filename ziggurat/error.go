5e711c23d6284242f3d13d7273d7a397f022e909
shubhang.balkundi <2932-shubhang.balkundi@users.noreply.source.golabs.io>
2020-08-17 09:42:45 +0530

    adds errors

--- ziggurat/error.go
+++ ziggurat/error.go
@@ -3,5 +3,7 @@ package ziggurat
 import "errors"
 
 var (
-	ErrNoHandlersRegistered = errors.New("error: no handlers registered")
+	ErrNoHandlersRegistered  = errors.New("error: no handlers registered")
+	ErrStreamRouteValidation = errors.New("config validation error,stream-routes count is 0")
+	ErrServiceNameValidation = errors.New("config validation error, service-name is empty")
 )
