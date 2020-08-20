96db4984184b5eeda85c07b848798c2872b985b2
shubhang.balkundi <2932-shubhang.balkundi@users.noreply.source.golabs.io>
2020-08-21 00:43:16 +0530

    removes unwanted constructor

--- ziggurat/messageEvent.go
+++ ziggurat/messageEvent.go
@@ -3,35 +3,23 @@ package ziggurat
 import "github.com/rs/zerolog/log"
 
 type MessageEvent struct {
-	MessageKey         interface{}
-	MessageValue       interface{}
-	MessageValueBytes  []byte
-	MessageKeyBytes    []byte
-	ConsumerInstanceId string
-	Topic              string
-	Partition          int
-	Attributes         map[string]interface {
-	}
-}
-
-func NewMessageEvent(key []byte, value []byte, topic string, partition int, consumerInstanceId string) MessageEvent {
-	return MessageEvent{
-		MessageKeyBytes:    key,
-		MessageValueBytes:  value,
-		Topic:              topic,
-		Partition:          partition,
-		ConsumerInstanceId: consumerInstanceId,
-	}
+	MessageKey        interface{}
+	MessageValue      interface{}
+	MessageValueBytes []byte
+	MessageKeyBytes   []byte
+	Topic             string
+	TopicEntity       string
+	attributes        map[string]interface{}
 }
 
 func (m MessageEvent) GetMessageAttribute(key string) interface{} {
-	return m.Attributes[key]
+	return m.attributes[key]
 }
 
 func (m *MessageEvent) SetMessageAttribute(key string, value interface{}) {
-	if m.Attributes[key] != nil {
+	if m.attributes[key] != nil {
 		log.Warn().Msgf("key %s already exists and will be overwritten", key)
-		m.Attributes[key] = value
+		m.attributes[key] = value
 	}
-	m.Attributes[key] = value
+	m.attributes[key] = value
 }
