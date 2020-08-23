56648af41c57ea5d769852c86fe8cafdab9d36da
shubhang.balkundi <2932-shubhang.balkundi@users.noreply.source.golabs.io>
2020-08-23 20:45:15 +0530

    removes retry payload type

--- ziggurat/retrier.go
+++ ziggurat/retrier.go
@@ -2,14 +2,6 @@ package ziggurat
 
 import "context"
 
-type RetryPayload struct {
-	MessageValueBytes []byte
-	MessageKeyBytes   []byte
-	TopicEntity       string
-	RetryCount        int
-	MessageTTL        string
-}
-
 type MessageRetrier interface {
 	Start(config Config, streamRoutes TopicEntityHandlerMap) error
 	Retry(config Config, payload MessageEvent) error
