68d2a692881715e99be62ce7876ee9f7e4d3bd8c
shubhang.balkundi <2932-shubhang.balkundi@users.noreply.source.golabs.io>
2020-08-26 20:50:29 +0530

    enables debug logs for consumer

--- ziggurat/router.go
+++ ziggurat/router.go
@@ -31,6 +31,7 @@ func newConsumerConfig() *kafka.ConfigMap {
 		"group.id":           "myGroup",
 		"auto.offset.reset":  "earliest",
 		"enable.auto.commit": false,
+		"debug":              "all",
 	}
 }
 
