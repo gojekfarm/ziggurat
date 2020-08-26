c1c9576cb4bd3a8e00a38512f86ea05f29e5c4fd
shubhang.balkundi <2932-shubhang.balkundi@users.noreply.source.golabs.io>
2020-08-26 09:42:17 +0530

    Revert "fixes logs kafka error"
    This reverts commit 9ac09dd8480b153364eedc2481c493ee1eca2704.

--- ziggurat/consumer.go
+++ ziggurat/consumer.go
@@ -41,10 +41,7 @@ func startConsumer(routerCtx context.Context, config Config, handlerFunc Handler
 				} else if err != nil && err.(kafka.Error).Code() == kafka.ErrAllBrokersDown {
 					log.Error().Err(err).Msg("terminating application, all brokers down")
 					return
-				} else if err != nil {
-					log.Error().Err(err).Msg("kafka error")
 				}
-
 				if msg != nil {
 					log.Info().Msgf("processing message for consumer %s", instanceID)
 					messageEvent := MessageEvent{
@@ -68,6 +65,7 @@ func startConsumer(routerCtx context.Context, config Config, handlerFunc Handler
 }
 
 func StartConsumers(routerCtx context.Context, config Config, consumerConfig *kafka.ConfigMap, topicEntity string, topics []string, instances int, handlerFunc HandlerFunc, retrier MessageRetrier, wg *sync.WaitGroup) []*kafka.Consumer {
+	fmt.Println("topic entity: ", topicEntity)
 	consumers := make([]*kafka.Consumer, 0, instances)
 	for i := 0; i < instances; i++ {
 		consumer := createConsumer(consumerConfig, topics)
