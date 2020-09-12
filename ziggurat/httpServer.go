d9b42c1f3cdbf224db73cc2b8dcb6d6118ba7ee4
shubhang.balkundi <2932-shubhang.balkundi@users.noreply.source.golabs.io>
2020-09-12 20:21:48 +0530

    adds http server module

--- ziggurat/httpServer.go
+++ ziggurat/httpServer.go
@@ -1,21 +1,41 @@
 package ziggurat
 
 import (
+	"context"
+	"fmt"
 	"github.com/julienschmidt/httprouter"
+	"github.com/rs/zerolog/log"
 	"net/http"
+	"strconv"
 )
 
-type HttpServer struct{}
+type HttpServer struct {
+	server  *http.Server
+	retrier MessageRetrier
+}
 
-func (s *HttpServer) Start(config Config, handlerMap TopicEntityHandlerMap) error {
+func (s *HttpServer) Start(ctx context.Context, config Config, retrier MessageRetrier, handlerMap TopicEntityHandlerMap) {
 	router := httprouter.New()
 	router.POST("/v1/dead_set/:topic_entity/:count", func(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
-
+		count, _ := strconv.Atoi(params.ByName("count"))
+		retrier.Replay(config, handlerMap, params.ByName("topic_entity"), count)
+		_, err := fmt.Fprintf(writer, "successfully replayed %d messages", count)
+		if err != nil {
+			writer.WriteHeader(500)
+		}
 	})
-	serverErr := http.ListenAndServe(":8080", router)
-	return serverErr
+
+	server := &http.Server{Addr: ":8080", Handler: router}
+	go func(server *http.Server) {
+		if err := server.ListenAndServe(); err != nil {
+			log.Fatal().Err(err)
+		}
+	}(server)
+
+	s.server = server
+
 }
 
 func (s *HttpServer) Stop() error {
-	return nil
+	return s.server.Shutdown(context.Background())
 }
