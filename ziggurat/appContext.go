fa2d0c1de30ab19921e6618638126ddc4c0f38f1
shubhang.balkundi <2932-shubhang.balkundi@users.noreply.source.golabs.io>
2020-09-13 15:42:08 +0530

    fixes types

--- ziggurat/appContext.go
+++ ziggurat/appContext.go
@@ -1,24 +1,8 @@
 package ziggurat
 
 type ApplicationContext struct {
-	httpServer   *Http
-	retrier      *MessageRetrier
-	config       *Config
-	streamRoutes TopicEntityHandlerMap
-}
-
-func (ac *ApplicationContext) attachHttpServer(server *Http) {
-	ac.httpServer = server
-}
-
-func (ac *ApplicationContext) attachRetrier(retrier *MessageRetrier) {
-	ac.retrier = retrier
-}
-
-func (ac *ApplicationContext) getHttpServer() *Http {
-	return ac.httpServer
-}
-
-func (ac *ApplicationContext) getRetrier() *MessageRetrier {
-	return ac.retrier
+	HttpServer   Http
+	Retrier      MessageRetrier
+	Config       Config
+	StreamRouter *StreamRouter
 }
