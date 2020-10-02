3d8c545e77794925ea8aff5946ae2871201a0ce2
shubhang.balkundi <2932-shubhang.balkundi@users.noreply.source.golabs.io>
2020-10-03 00:12:35 +0530

    published success metric

--- ziggurat/handler.go
+++ ziggurat/handler.go
@@ -6,9 +6,17 @@ import (
 
 func MessageHandler(applicationContext ApplicationContext, handlerFunc HandlerFunc, retrier MessageRetrier) func(event MessageEvent) {
 	return func(event MessageEvent) {
+		metricTags := map[string]string{
+			"topic_entity": event.TopicEntity,
+			"kafka_topic":  event.Topic,
+		}
 		status := handlerFunc(event)
 		switch status {
 		case ProcessingSuccess:
+			if publishErr := applicationContext.MetricPublisher.IncCounter("message_processing_success", 1, metricTags); publishErr != nil {
+				log.Error().Err(publishErr).Msg("")
+			}
+
 			log.Info().Msg("successfully processed message")
 		case SkipMessage:
 			log.Info().Msg("skipping message")
