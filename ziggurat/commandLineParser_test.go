621220ada0c256fc32383f292401dba0f5ef71e7
shubhang.balkundi <2932-shubhang.balkundi@users.noreply.source.golabs.io>
2020-10-04 11:30:22 +0530

    adds tests for parsing cmd line arguments

--- ziggurat/commandLineParser_test.go
+++ ziggurat/commandLineParser_test.go
@@ -1,1 +1,24 @@
 package ziggurat
+
+import (
+	"os"
+	"testing"
+)
+
+func TestParseCommandLineArgumentsWithDefaultValues(t *testing.T) {
+	expected := CommandLineOptions{ConfigFilePath: "./config/config.yaml"}
+	cmdOptions := ParseCommandLineArguments()
+	if expected != cmdOptions {
+		t.Errorf("EXPECTED %+v GOT %+v", expected, cmdOptions)
+	}
+
+}
+
+func TestParseCommandLineArguments(t *testing.T) {
+	os.Args = append(os.Args, "--ziggurat-config=overriddenPath")
+	cmdOptions := ParseCommandLineArguments()
+	newOptions := CommandLineOptions{ConfigFilePath: "overriddenPath"}
+	if newOptions != cmdOptions {
+		t.Errorf("FAILED got %+v EXPECTED %+v", cmdOptions, newOptions)
+	}
+}
