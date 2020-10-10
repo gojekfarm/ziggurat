33d02f233f8b656c1aad4312120551e1ad43659f
shubhang.balkundi <2932-shubhang.balkundi@users.noreply.source.golabs.io>
2020-10-10 20:57:08 +0530

    fixes middleware func signature

--- ziggurat/middleware.go
+++ ziggurat/middleware.go
@@ -7,28 +7,28 @@ import (
 )
 
 func JSONDeserializer(handlerFn HandlerFunc, structValue interface{}) HandlerFunc {
-	return func(message MessageEvent) ProcessStatus {
+	return func(message MessageEvent, app *App) ProcessStatus {
 		messageValueBytes := message.MessageValueBytes
 		if err := json.Unmarshal(messageValueBytes, structValue); err != nil {
 			log.Error().Err(err).Msg("error deserializing json")
 			message.MessageValue = nil
-			return handlerFn(message)
+			return handlerFn(message, app)
 		}
 		message.MessageValue = structValue
-		return handlerFn(message)
+		return handlerFn(message, app)
 	}
 }
 
 func ProtobufDeserializer(handler HandlerFunc, messageValue proto.Message) HandlerFunc {
-	return func(messageEvent MessageEvent) ProcessStatus {
+	return func(messageEvent MessageEvent, app *App) ProcessStatus {
 		messageValueBytes := messageEvent.MessageValueBytes
 		if err := proto.Unmarshal(messageValueBytes, messageValue); err != nil {
 			messageEvent.MessageValue = messageValue
 			log.Error().Err(err).Msg("error deserializing protobuf")
 			messageEvent.MessageValue = nil
-			return handler(messageEvent)
+			return handler(messageEvent, app)
 		}
 		messageEvent.MessageValue = messageValue
-		return handler(messageEvent)
+		return handler(messageEvent, app)
 	}
 }
