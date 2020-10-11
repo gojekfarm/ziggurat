b3a043ebaee4be70fe03063ae3dc8f583d9c9b7d
shubhang.balkundi <2932-shubhang.balkundi@users.noreply.source.golabs.io>
2020-10-11 19:53:56 +0530

    adds new error

--- zig/error.go
+++ zig/error.go
@@ -13,4 +13,5 @@ var (
 	ErrReplayCountZero        = errors.New("replay count is 0, cannot 0 messages")
 	ErrInvalidReturnCode      = errors.New("invalid processing function return code")
 	ErrInvalidRouteRegistered = errors.New("invalid route registered")
+	ErrInvalidMiddlewareKey   = errors.New("wrong topic entity provided to Use")
 )
