d6da9e1604ea449c73cd3678db62066baf503da1
shubhang.balkundi <2932-shubhang.balkundi@users.noreply.source.golabs.io>
2020-10-14 21:46:16 +0530

    extract public interfaces to types

--- zig/retrier.go
+++
@@ -1,8 +0,0 @@
-package zig
-
-type MessageRetrier interface {
-	Start(app *App) (chan int, error)
-	Retry(app *App, payload MessageEvent) error
-	Stop() error
-	Replay(app *App, topicEntity string, count int) error
-}
--- zig/types.go
+++ zig/types.go
@@ -1,11 +1,32 @@
 package zig
 
+//public types
+type ProcessStatus int
 type HandlerFunc func(messageEvent MessageEvent, app *App) ProcessStatus
 type StartFunction func(a *App)
 type StopFunction func()
-type ProcessStatus int
 type Middleware func(next HandlerFunc) HandlerFunc
 
+// Public interfaces
+type HttpServer interface {
+	Start(app *App)
+	Stop() error
+}
+
+type MetricPublisher interface {
+	Start(app *App) error
+	Stop() error
+	IncCounter(metricName string, value int, arguments map[string]string) error
+}
+
+type MessageRetrier interface {
+	Start(app *App) (chan int, error)
+	Retry(app *App, payload MessageEvent) error
+	Stop() error
+	Replay(app *App, topicEntity string, count int) error
+}
+
+// Public constants
 const ProcessingSuccess ProcessStatus = 0
 const RetryMessage ProcessStatus = 1
 const SkipMessage ProcessStatus = 2
