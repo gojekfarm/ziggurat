b3bd34fea71799a4924e60ec106e13e95a472d71
shubhang.balkundi <2932-shubhang.balkundi@users.noreply.source.golabs.io>
2020-10-16 09:07:50 +0530

    creates a new function to compose

--- zig/util.go
+++ zig/util.go
@@ -1,6 +1,24 @@
 package zig
 
-func pipeHandlers(funcs ...MiddlewareFunc) func(handlerFunc HandlerFunc) HandlerFunc {
+func PipeHandlers(funcs ...MiddlewareFunc) func(handlerFunc HandlerFunc) HandlerFunc {
+	return func(origHandler HandlerFunc) HandlerFunc {
+		var handlerResult HandlerFunc
+		return func(messageEvent MessageEvent, app *App) ProcessStatus {
+			last := len(funcs) - 1
+			for i := last; i >= 0; i-- {
+				f := funcs[i]
+				if i == last {
+					handlerResult = f(origHandler)
+				} else {
+					handlerResult = f(handlerResult)
+				}
+			}
+			return handlerResult(messageEvent, app)
+		}
+	}
+}
+
+func ComposeHandlers(funcs ...MiddlewareFunc) func(handlerFunc HandlerFunc) HandlerFunc {
 	return func(origHandler HandlerFunc) HandlerFunc {
 		var handlerResult HandlerFunc
 		return func(messageEvent MessageEvent, app *App) ProcessStatus {
