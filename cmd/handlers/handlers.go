aa1cd3fd7518f0cee4b27862c220b47724fb0fb9
shubhang.balkundi <2932-shubhang.balkundi@users.noreply.source.golabs.io>
2020-11-06 10:41:55 +0530

    refactor cmd packages

---
+++ cmd/handlers/handlers.go
@@ -0,0 +1,38 @@
+package handlers
+
+import (
+	"fmt"
+	"github.com/gojekfarm/ziggurat-go/cmd"
+)
+
+var NewHandler = func(args []string) int {
+	if len(args) < 2 {
+		usage := `[USAGE]
+zig new <app_name>`
+		fmt.Println(usage)
+		return 1
+	}
+	appName := args[1]
+	d := cmd.Data{
+		AppName:       appName,
+		TopicEntity:   "my-topic-entity",
+		ConsumerGroup: appName + "_" + "consumer",
+	}
+	tplConfig := cmd.GetTemplateConfig()
+	zts := cmd.NewZigTemplateSet(appName, tplConfig)
+
+	if err := zts.Parse(); err != nil {
+		fmt.Println("command failed with error:", err)
+		return 1
+	}
+	if err := zts.CreateOutFiles(); err != nil {
+		fmt.Println("command failed with error:", err)
+		return 1
+	}
+	if err := zts.Render(d); err != nil {
+		fmt.Println("command failed with error:", err)
+		return 1
+	}
+	fmt.Println("created app ", appName)
+	return 0
+}
--- cmd/zig/main.go
+++ cmd/zig/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"github.com/gojekfarm/ziggurat-go/cmd"
+	"github.com/gojekfarm/ziggurat-go/cmd/handlers"
 	"os"
 )
 
@@ -10,36 +10,6 @@ func main() {
 	cli := cmd.NewCLI("zig")
 	cli.AddUsage(`[USAGE]
 zig command_name <args>`)
-	cli.AddCommand("new", func(args []string) int {
-		if len(args) < 2 {
-			usage := `[USAGE]
-zig new <app_name>`
-			fmt.Println(usage)
-			return 1
-		}
-		appName := args[1]
-		d := cmd.Data{
-			AppName:       appName,
-			TopicEntity:   "my-topic-entity",
-			ConsumerGroup: appName + "_" + "consumer",
-		}
-		tplConfig := cmd.GetTemplateConfig()
-		zts := cmd.NewZigTemplateSet(appName, tplConfig)
-
-		if err := zts.Parse(); err != nil {
-			fmt.Println("command failed with error:", err)
-			return 1
-		}
-		if err := zts.CreateOutFiles(); err != nil {
-			fmt.Println("command failed with error:", err)
-			return 1
-		}
-		if err := zts.Render(d); err != nil {
-			fmt.Println("command failed with error:", err)
-			return 1
-		}
-		fmt.Println("created app ", appName)
-		return 0
-	})
+	cli.AddCommand("new", handlers.NewHandler)
 	cli.Run(os.Args)
 }
