9b7e4cf0376e4104ff47e0e506617bdffe95ff29
shubhang.balkundi <2932-shubhang.balkundi@users.noreply.source.golabs.io>
2020-12-07 18:12:26 +0530

    adds tests for http middleware

---
+++ pkg/server/mw_test.go
@@ -0,0 +1,22 @@
+package server
+
+import (
+	"github.com/gojekfarm/ziggurat-go/pkg/zlog"
+	"net/http"
+	"net/http/httptest"
+	"reflect"
+	"testing"
+)
+
+func TestRequestLoggerMW(t *testing.T) {
+	expectedArgs := map[string]interface{}{"METHOD": "GET", "URL": "/test"}
+	zlog.LogInfo = func(msg string, args map[string]interface{}) {
+		if !reflect.DeepEqual(expectedArgs, args) {
+			t.Errorf("expected args %v got %v", expectedArgs, args)
+		}
+	}
+	rl := requestLogger(http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {}))
+	recorder := httptest.NewRecorder()
+	req := httptest.NewRequest("GET", "/test", nil)
+	rl.ServeHTTP(recorder, req)
+}
