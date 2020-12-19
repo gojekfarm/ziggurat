aa02d4f10390c52b36eefbd0565aa9b72ebc8838
shubhang.balkundi <2932-shubhang.balkundi@users.noreply.source.golabs.io>
2020-12-19 18:24:46 +0530

    removes rmq middleware and statsd middleware

--- example/main.go
+++ example/main.go
@@ -4,9 +4,6 @@ import (
 	"context"
 	"github.com/gojekfarm/ziggurat"
 	"github.com/gojekfarm/ziggurat/mw"
-	"github.com/gojekfarm/ziggurat/mw/metrics"
-	"github.com/gojekfarm/ziggurat/mw/retry"
-	"github.com/gojekfarm/ziggurat/server"
 )
 
 const RoutePlainTextLog = "plain-text-log"
@@ -15,23 +12,15 @@ func main() {
 	app := &ziggurat.Ziggurat{}
 	router := ziggurat.NewRouter()
 	loggerMW := mw.NewProcessingStatusLogger()
-	retryMW := retry.NewRabbitRetrier(
-		[]string{"amqp://user:bitnami@localhost:5672"},
-		retry.QueueConfig{RoutePlainTextLog: {DelayQueueExpirationInMS: "500", RetryCount: 2}}, nil)
-	statsDClient := metrics.NewClient(metrics.WithPrefix("super-app"))
-	httpServer := server.NewHTTPServer()
 
 	router.HandleFunc(RoutePlainTextLog, func(event *ziggurat.Message, ctx context.Context) ziggurat.ProcessStatus {
 		return ziggurat.RetryMessage
 	})
-	
-	handler := router.Compose(loggerMW.LogStatus, statsDClient.PublishKafkaLag, statsDClient.PublishHandlerMetrics, retryMW.Retrier)
+
+	handler := router.Compose(loggerMW.LogStatus)
 
 	app.StartFunc(func(ctx context.Context) {
-		retryMW.RunPublisher(ctx)
-		retryMW.RunConsumers(ctx, handler)
-		statsDClient.Run(ctx)
-		httpServer.Run(ctx)
+
 	})
 
 	<-app.Run(context.Background(), handler,
