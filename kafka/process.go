cd18c55e53151132e6f5107049301bafb9afe227
shubhang.balkundi <2932-shubhang.balkundi@users.noreply.source.golabs.io>
2021-04-09 14:41:31 +0530

    adds stores kafka metadata in headers

--- kafka/process.go
+++ kafka/process.go
@@ -2,23 +2,37 @@ package kafka
 
 import (
 	"context"
+	"strconv"
 
 	"github.com/confluentinc/confluent-kafka-go/kafka"
 	"github.com/gojekfarm/ziggurat"
 )
 
 func kafkaProcessor(msg *kafka.Message, route string, c *kafka.Consumer, h ziggurat.Handler, l ziggurat.StructuredLogger, ctx context.Context) {
+	timestamp := msg.Timestamp.String()
+	topic := *msg.TopicPartition.Topic
+	partition := int(msg.TopicPartition.Partition)
+
 	headers := map[string]string{
 		ziggurat.HeaderMessageRoute: route,
 		ziggurat.HeaderMessageType:  "kafka",
+		"x-kafka-timestamp":         timestamp,
+		"x-kafka-topic":             topic,
+		"x-kafka-partition":         strconv.Itoa(partition),
 	}
 
 	event := NewMessage(msg.Value, msg.Key, headers)
-	event.Timestamp = msg.Timestamp
-	event.Topic = *msg.TopicPartition.Topic
-	event.Partition = int(msg.TopicPartition.Partition)
 
-	h.Handle(ctx, event)
+	kvs := map[string]interface{}{
+		"kafka-topic": topic,
+		"partition":   partition,
+		"route":       route,
+	}
+	if handlerError := h.Handle(ctx, event); handlerError != nil {
+		l.Error("error processing message", handlerError, kvs)
+	} else {
+		l.Info("processed message successfully", kvs)
+	}
 	err := storeOffsets(c, msg.TopicPartition)
 	l.Error("error storing offsets: %v", err)
 }
