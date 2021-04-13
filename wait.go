606627357c4e50a9937d8f1d26c4ebc69deb8bac
shubhang93 <shubhang.sabbath@gmail.com>
2021-04-13 16:08:30 +0530

    fixes comment formatting

--- wait.go
+++ wait.go
@@ -2,7 +2,7 @@ package ziggurat
 
 import "sync"
 
-// WaitAll is a util function which takes in n number of channels
+// WaitAll is a util function which takes in n channels
 // and waits on all of them
 // every channel must be of type chan struct{}
 func WaitAll(chans ...chan struct{}) {
