9e2496cbeca3ea7fc02f11c406a088ebec7650db
shubhang.balkundi <2932-shubhang.balkundi@users.noreply.source.golabs.io>
2021-04-16 17:42:08 +0530

    fixes return value in prom middleware

--- mw/prometheus/prometheus.go
+++ mw/prometheus/prometheus.go
@@ -81,7 +81,7 @@ func Register() {
 
 // PublishHandlerMetrics - middleware to update registered handler metrics
 func PublishHandlerMetrics(next ziggurat.Handler) ziggurat.Handler {
-	f := func(ctx context.Context, event ziggurat.Event) error {
+	f := func(ctx context.Context, event ziggurat.Event) interface{} {
 		t1 := time.Now()
 		err := next.Handle(ctx, event)
 
