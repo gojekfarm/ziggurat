0b6969558c4bc30aa3fb659e2f5ebf67db1c9489
shubhang93 <shubhang.sabbath@gmail.com>
2021-04-18 23:10:21 +0530

    updates example

--- mw/event/logger.go
+++ mw/event/logger.go
@@ -9,13 +9,19 @@ import (
 func Logger(l ziggurat.StructuredLogger) func(handler ziggurat.Handler) ziggurat.Handler {
 	return func(handler ziggurat.Handler) ziggurat.Handler {
 		f := func(ctx context.Context, event *ziggurat.Event) error {
-			l.Info("received message", map[string]interface{}{
+			kvs := map[string]interface{}{
 				"path":               event.Path,
 				"producer-timestamp": event.ProducerTimestamp,
 				"received-timestamp": event.ReceivedTimestamp,
 				"value-length":       len(event.Value),
 				"event-type":         event.EventType,
-			})
+			}
+
+			for k, v := range event.Headers {
+				kvs[k] = v
+			}
+
+			l.Info("received message", kvs)
 			return handler.Handle(ctx, event)
 		}
 		return ziggurat.HandlerFunc(f)
