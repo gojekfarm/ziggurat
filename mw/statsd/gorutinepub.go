96deb76a7bcb0b51e244e9d480b1ba62bf3a356d
shubhang93 <shubhang.sabbath@gmail.com>
2021-05-17 21:18:22 +0530

    reverts metrics read from metrics package

--- mw/statsd/gorutinepub.go
+++ mw/statsd/gorutinepub.go
@@ -2,9 +2,7 @@ package statsd
 
 import (
 	"context"
-	"errors"
 	"runtime"
-	"runtime/metrics"
 	"time"
 )
 
@@ -13,26 +11,12 @@ func goRoutinePublisher(ctx context.Context, interval time.Duration, s *Client) 
 	t := time.NewTicker(interval)
 	tickerChan := t.C
 
-	const metricName = "/sched/goroutines:goroutines"
-	sample := make([]metrics.Sample, 1)
-	sample[0].Name = metricName
-
 	for {
 		select {
 		case <-done:
 			t.Stop()
 			return
 		case <-tickerChan:
-			metrics.Read(sample)
-			if sample[0].Value.Kind() == metrics.KindBad {
-				s.logger.Error("error publishing metric", errors.New("bad metric kind"))
-			} else {
-				s.logger.Error("error publishing goroutine count",
-					s.Gauge("num_alive_goroutine",
-						int64(sample[0].Value.Uint64()),
-						nil),
-				)
-			}
 			s.logger.Error("error publishing go-routine count",
 				s.Gauge("num_goroutine",
 					int64(runtime.NumGoroutine()),
