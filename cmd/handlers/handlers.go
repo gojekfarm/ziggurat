954cda8349578853e4f69f0eb97749cad7305cf8
shubhang.balkundi <2932-shubhang.balkundi@users.noreply.source.golabs.io>
2021-07-05 16:44:05 +0530

    bumps ziggurat version

--- cmd/handlers/handlers.go
+++ cmd/handlers/handlers.go
@@ -20,7 +20,7 @@ zig new <app_name>`
 		ConsumerGroup:    appName + "_" + "consumer",
 		OriginTopics:     "plain-text-log",
 		BootstrapServers: "localhost:9092",
-		ZigguratVersion:  "v1.2.0",
+		ZigguratVersion:  "v1.2.1",
 	}
 	tplConfig := cmd.GetTemplateConfig()
 	zts := cmd.NewZigTemplateSet(appName, tplConfig)
