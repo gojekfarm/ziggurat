86bc20c574f621566f0af76f9ceaecbc51b9b2fc
shubhang.balkundi <2932-shubhang.balkundi@users.noreply.source.golabs.io>
2021-07-05 16:09:00 +0530

    updates ziggurat version in the template

--- cmd/templates/templates.go
+++ cmd/templates/templates.go
@@ -64,7 +64,7 @@ var GoMod = `module {{.AppName}}
 go 1.16
 
 require (
-	github.com/gojekfarm/ziggurat v1.1.1
+	github.com/gojekfarm/ziggurat v1.2.0
 	github.com/julienschmidt/httprouter v1.2.0
 )`
 
