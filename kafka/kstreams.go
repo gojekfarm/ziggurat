974009a616bf3b5f8b4f17608933f17c88f11018
shubhang.balkundi <2932-shubhang.balkundi@users.noreply.source.golabs.io>
2021-08-16 08:59:01 +0530

    adds rabbitmq publishing and tests

--- kafka/kstreams.go
+++ kafka/kstreams.go
@@ -58,7 +58,7 @@ func (k *Streams) Stream(ctx context.Context, handler ziggurat.Handler) error {
 func (k *Streams) stop() {
 	for _, consumers := range k.routeConsumerMap {
 		for i := range consumers {
-			k.Logger.Error("error stopping consumer: %v", consumers[i].Close(), nil)
+			k.Logger.Error("error stopping consumer: %v", consumers[i].Close())
 		}
 	}
 }
