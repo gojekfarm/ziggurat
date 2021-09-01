fbd4353ce5d70a9e38d00cf3048f395eaff0b16a
shubhang.balkundi <2932-shubhang.balkundi@users.noreply.source.golabs.io>
2021-09-01 18:33:37 +0530

    changes logger name

--- logger/logger.go
+++ logger/logger.go
@@ -6,35 +6,35 @@ import (
 	"github.com/rs/zerolog"
 )
 
-type humanLogger struct {
+type textLogger struct {
 	l zerolog.Logger
 }
 
-func (h *humanLogger) Info(message string, kvs ...map[string]interface{}) {
+func (h *textLogger) Info(message string, kvs ...map[string]interface{}) {
 	appendFields(h.l.Info(), kvs).Msg(message)
 }
 
-func (h *humanLogger) Debug(message string, kvs ...map[string]interface{}) {
+func (h *textLogger) Debug(message string, kvs ...map[string]interface{}) {
 	appendFields(h.l.Debug(), kvs).Msg(message)
 }
 
-func (h *humanLogger) Warn(message string, kvs ...map[string]interface{}) {
+func (h *textLogger) Warn(message string, kvs ...map[string]interface{}) {
 	appendFields(h.l.Warn(), kvs).Msg(message)
 }
 
-func (h *humanLogger) Error(message string, err error, kvs ...map[string]interface{}) {
+func (h *textLogger) Error(message string, err error, kvs ...map[string]interface{}) {
 	if err != nil {
 		appendFields(h.l.Err(err), kvs).Msg(message)
 	}
 }
 
-func (h *humanLogger) Fatal(message string, err error, kvs ...map[string]interface{}) {
+func (h *textLogger) Fatal(message string, err error, kvs ...map[string]interface{}) {
 	if err != nil {
 		appendFields(h.l.Fatal(), kvs).Msg(message)
 	}
 }
 
-func NewLogger(level string, opts ...func(w *zerolog.ConsoleWriter)) *humanLogger {
+func NewLogger(level string, opts ...func(w *zerolog.ConsoleWriter)) *textLogger {
 	cw := zerolog.NewConsoleWriter(func(w *zerolog.ConsoleWriter) {
 		w.FormatLevel = func(i interface{}) string {
 			return fmt.Sprintf("[%s]", i)
@@ -46,5 +46,5 @@ func NewLogger(level string, opts ...func(w *zerolog.ConsoleWriter)) *humanLogge
 		}
 	})
 	l := zerolog.New(cw).With().Timestamp().Logger().Level(logLevelMapping[level])
-	return &humanLogger{l: l}
+	return &textLogger{l: l}
 }
