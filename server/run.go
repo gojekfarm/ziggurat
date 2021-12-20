ce68fec07b500dedf6c0721165649d39e7fc66d0
shubhang.balkundi <shubhang.balkundi@go-jek.com>
2021-12-20 17:13:43 +0530

    adds a server pkg with Run function

---
+++ server/run.go
@@ -0,0 +1,22 @@
+package server
+
+import (
+	"context"
+	"net/http"
+)
+
+func Run(ctx context.Context, s *http.Server) error {
+	errChan := make(chan error)
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			errChan <- err
+		}
+	}()
+	select {
+	case <-ctx.Done():
+		return s.Shutdown(ctx)
+	case err := <-errChan:
+		return err
+	}
+}
