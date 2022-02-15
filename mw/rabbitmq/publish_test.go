d4a4d9507945e19218152d58d8e80ad284999eda
shubhang.balkundi <shubhang.balkundi@go-jek.com>
2022-02-15 12:53:36 +0530

    fixes unhandled error in test

--- mw/rabbitmq/publish_test.go
+++ mw/rabbitmq/publish_test.go
@@ -43,7 +43,7 @@ func Test_publish(t *testing.T) {
 			}
 			return nil
 		}
-		publishInternal(&p, "foo", c.retryCount, "100", c.input)
+		_ = publishInternal(&p, "foo", c.retryCount, "100", c.input)
 	}
 }
 
@@ -82,7 +82,7 @@ func Test_publish_retries(t *testing.T) {
 		t.Run(c.name, func(t *testing.T) {
 			e := ziggurat.Event{}
 			for i := 0; i < c.publishIterations; i++ {
-				publishInternal(&p, "", c.retryCount, "", &e)
+				_ = publishInternal(&p, "", c.retryCount, "", &e)
 			}
 			if c.expectedCount != e.Metadata[KeyRetryCount] {
 				t.Errorf("expected %v got %v", c.expectedCount, e.Metadata[KeyRetryCount])
