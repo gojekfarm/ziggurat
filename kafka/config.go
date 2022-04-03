418341bf61268ba763bb7aa6ac4df759e4c46b7d
shubhang.balkundi <shubhang.balkundi@go-jek.com>
2022-04-03 17:38:30 +0530

    cleans up code

--- kafka/config.go
+++ kafka/config.go
@@ -3,47 +3,47 @@ package kafka
 import "github.com/confluentinc/confluent-kafka-go/kafka"
 
 type ConsumerConfig struct {
-    BootstrapServers    string
-    DebugLevel          string
-    GroupID             string
-    Topics              string
-    AutoCommitInterval  int
-    ConsumerCount       int
-    PollTimeout         int
-    RouteGroup          string
-    AutoOffsetReset     string
-    PartitionAssignment string
+	BootstrapServers    string
+	DebugLevel          string
+	GroupID             string
+	Topics              string
+	AutoCommitInterval  int
+	ConsumerCount       int
+	PollTimeout         int
+	RouteGroup          string
+	AutoOffsetReset     string
+	PartitionAssignment string
 }
 
 func (c ConsumerConfig) toConfigMap() kafka.ConfigMap {
 
-    kafkaConfMap := kafka.ConfigMap{
-        "bootstrap.servers":        c.BootstrapServers,
-        "group.id":                 c.GroupID,
-        "enable.auto.commit":       true,
-        "debug":                    "consumer",
-        "go.logs.channel.enable":   true,
-        "enable.auto.offset.store": false,
-        "auto.commit.interval.ms":  15000,
-        "auto.offset.reset":        "earliest",
-    }
-    if c.AutoOffsetReset != "" {
-        kafkaConfMap["auto.offset.reset"] = c.AutoOffsetReset
-    }
+	kafkaConfMap := kafka.ConfigMap{
+		"bootstrap.servers":        c.BootstrapServers,
+		"group.id":                 c.GroupID,
+		"enable.auto.commit":       true,
+		"debug":                    "consumer",
+		"go.logs.channel.enable":   true,
+		"enable.auto.offset.store": false,
+		"auto.commit.interval.ms":  15000,
+		"auto.offset.reset":        "earliest",
+	}
+	if c.AutoOffsetReset != "" {
+		kafkaConfMap["auto.offset.reset"] = c.AutoOffsetReset
+	}
 
-    if c.PartitionAssignment != "" {
-        kafkaConfMap["partition.assignment.strategy"] = c.PartitionAssignment
-    }
+	if c.PartitionAssignment != "" {
+		kafkaConfMap["partition.assignment.strategy"] = c.PartitionAssignment
+	}
 
-    if c.DebugLevel != "" {
-        kafkaConfMap["debug"] = c.DebugLevel
-    }
+	if c.DebugLevel != "" {
+		kafkaConfMap["debug"] = c.DebugLevel
+	}
 
-    if c.AutoCommitInterval > 0 {
-        kafkaConfMap["auto.commit.interval.ms"] = c.AutoCommitInterval
-    }
+	if c.AutoCommitInterval > 0 {
+		kafkaConfMap["auto.commit.interval.ms"] = c.AutoCommitInterval
+	}
 
-    return kafkaConfMap
+	return kafkaConfMap
 }
 
 type StreamConfig = []ConsumerConfig
--- mw/rabbitmq/pubpool.go
+++ mw/rabbitmq/pubpool.go
@@ -1,33 +1,33 @@
 package rabbitmq
 
 import (
-    "context"
+	"context"
 
-    "github.com/makasim/amqpextra"
-    "github.com/makasim/amqpextra/logger"
-    "github.com/makasim/amqpextra/publisher"
+	"github.com/makasim/amqpextra"
+	"github.com/makasim/amqpextra/logger"
+	"github.com/makasim/amqpextra/publisher"
 )
 
 type publisherPool struct {
-    pool chan *publisher.Publisher
+	pool chan *publisher.Publisher
 }
 
 func newPubPool(ctx context.Context, size int, d *amqpextra.Dialer, logger logger.Logger) (*publisherPool, error) {
-    cpool := &publisherPool{pool: make(chan *publisher.Publisher, size)}
-    for i := 0; i < size; i++ {
-        p, err := getPublisher(ctx, d, logger)
-        if err != nil {
-            return nil, err
-        }
-        cpool.put(p)
-    }
-    return cpool, nil
+	pubPool := &publisherPool{pool: make(chan *publisher.Publisher, size)}
+	for i := 0; i < size; i++ {
+		p, err := getPublisher(ctx, d, logger)
+		if err != nil {
+			return nil, err
+		}
+		pubPool.put(p)
+	}
+	return pubPool, nil
 }
 
 func (c *publisherPool) get() *publisher.Publisher {
-    return <-c.pool
+	return <-c.pool
 }
 
 func (c *publisherPool) put(p *publisher.Publisher) {
-    c.pool <- p
+	c.pool <- p
 }
