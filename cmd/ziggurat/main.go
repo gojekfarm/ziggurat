1e8756bc5085d458ae4d2362a57d975ef2d46e86
shubhang.balkundi <shubhang.balkundi@go-jek.com>
2023-10-20 13:55:39 +0530

    updates version to v1.7.4

--- cmd/ziggurat/main.go
+++ cmd/ziggurat/main.go
@@ -44,7 +44,7 @@ func main() {
 		die(errors.New(usage))
 	}
 	appName := args[1]
-	d := Data{AppName: appName, Version: "v1.7.2"}
+	d := Data{AppName: appName, Version: "v1.7.4"}
 	wd, err := os.Getwd()
 	die(err)
 
