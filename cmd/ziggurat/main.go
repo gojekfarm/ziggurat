1ed288d45a76d09c21de5e269648463ff39eee80
Shubhang Balkundi <shubhang.balkundi@go-jek.com>
2024-03-26 17:17:19 +0530

    updates version

--- cmd/ziggurat/main.go
+++ cmd/ziggurat/main.go
@@ -44,7 +44,7 @@ func main() {
 		die(errors.New(usage))
 	}
 	appName := args[1]
-	d := Data{AppName: appName, Version: "v2.0.1"}
+	d := Data{AppName: appName, Version: "v2.0.10"}
 	wd, err := os.Getwd()
 	die(err)
 
